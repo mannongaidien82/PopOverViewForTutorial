5fd7882c0a7ae30ca9cde1cbcb0115a3220aec42
HIkaruSato <sato.hikaru.dev@gmail.com>
2016-07-17 12:04:16 +0900

    Initial Commit

---
+++ PopOverViewForTutorial/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PopOverViewForTutorial
+//
+//  Created by HikaruSato on 2016/07/17.
+//  Copyright © 2016年 HikaruSato. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PopOverViewForTutorial/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PopOverViewForTutorial
+//
+//  Created by HikaruSato on 2016/07/17.
+//  Copyright © 2016年 HikaruSato. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
